ffdb2aae2326e67d6dd9521e3a05d399c2718434
beastmond <beastmond@bkeastmond.bke>
2016-02-20 23:58:30 -0700

    Missed this checkin

---
+++ 5485-StrongHold/src/org/usfirst/frc/team5485/robot/PortsButtons.java
@@ -0,0 +1,19 @@
+package org.usfirst.frc.team5485.robot;
+
+public class PortsButtons {
+	public final static int MC_DRIVE_RIGHT = 2;
+	public final static int MC_DRIVE_LEFT = 1;
+	public final static int MC_BALL_SHOOTER_RIGHT = 8;
+	public final static int MC_BALL_SHOOTER_LEFT = 7;
+	public final static int MC_BALL_INTAKE = 9;
+	
+	public final static int JOYBTN_A = 1;
+	public final static int JOYBTN_B = 2;
+	public final static int JOYBTN_X = 3;
+	public final static int JOYBTN_Y = 4;
+	public final static int JOYBTN_LB = 5;
+	public final static int JOYBTN_RB = 6;
+	
+	public final static int JOYAXS_LEFT_Y = 1;
+	public final static int JOYAXS_RIGHT_Y = 5;
+}
